b2b2762899e757e5d2861c39158ecd72398dd094
fverin-dev <francois.verin@epitech.eu>
2023-01-30 19:11:31 +0100

    feat: add index.js with the base code of bot

---
+++ index.js
@@ -0,0 +1,60 @@
+require('dotenv').config()
+
+const fs = require('node:fs')
+const path = require('node:path')
+// Require the necessary discord.js classes
+const { Client, Collection, Events, GatewayIntentBits } = require('discord.js')
+
+const { BOT_TOKEN } = process.env
+
+// Create a new client instance
+const client = new Client({ intents: [GatewayIntentBits.Guilds] })
+
+client.commands = new Collection()
+
+const commandsPath = path.join(__dirname, 'commands')
+const commandFiles = fs.readdirSync(commandsPath).filter(file => file.endsWith('.js'))
+
+// eslint-disable-next-line no-restricted-syntax
+for (const file of commandFiles) {
+  const filePath = path.join(commandsPath, file)
+  // eslint-disable-next-line import/no-dynamic-require, global-require
+  const command = require(filePath)
+  // Set a new item in the Collection with the key as the command name and the value as the exported module
+  if ('data' in command && 'execute' in command) {
+    client.commands.set(command.data.name, command)
+  } else {
+    // eslint-disable-next-line no-console
+    console.log(`[WARNING] The command at ${filePath} is missing a required "data" or "execute" property.`)
+  }
+}
+
+// When the client is ready, run this code (only once)
+// We use 'c' for the event parameter to keep it separate from the already defined 'client'
+client.once(Events.ClientReady, c => {
+  // eslint-disable-next-line no-console
+  console.log(`Ready! Logged in as ${c.user.tag}`)
+})
+
+client.on(Events.InteractionCreate, async interaction => {
+  if (!interaction.isChatInputCommand()) return
+
+  const command = interaction.client.commands.get(interaction.commandName)
+
+  if (!command) {
+    // eslint-disable-next-line no-console
+    console.error(`No command matching ${interaction.commandName} was found.`)
+    return
+  }
+
+  try {
+    await command.execute(interaction)
+  } catch (error) {
+    // eslint-disable-next-line no-console
+    console.error(error)
+    await interaction.reply({ content: 'There was an error while executing this command!', ephemeral: true })
+  }
+})
+
+// Log in to Discord with your client's token
+client.login(BOT_TOKEN)
